c0d7e6c2e3f11ce6e43402318d04d3c3d52d398e
akshaybhagwat76 <akshaybhagwat76@gmail.com>
2020-05-29 09:40:11 +0530

    added multiple y axis

--- src/app/assets-chart/assets-chart.component.ts
+++ src/app/assets-chart/assets-chart.component.ts
@@ -26,7 +26,7 @@ chart.paddingRight = 40;
 
 var data = [];
 var value = 50;
-for (let i = -7300; i < 0; i++) {
+for (let i = -730; i < 0; i++) {
   for(let j = 0; j < 24; j++)
   {
     let date = new Date();
@@ -37,34 +37,84 @@ for (let i = -7300; i < 0; i++) {
     value = Math.round(Math.random() * 10);
   }
   data.push({ date: date, value: value });
+} 
+}
 
+for (let i = -730; i < 0; i++) {
+  for(let j = 0; j < 24; j++)
+  {
+    let date = new Date();
+  date.setHours(j, 0, 0, 0);
+  date.setDate(i);
+  value -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+  if (value < 0) {
+    value = Math.round(Math.random() * 10);
   }
-  
+  data.push({ date2: date, value2: value });
+} 
 }
 
 chart.data = data;
 
-// Create axes
 var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-dateAxis.renderer.minGridDistance = 60;
-dateAxis.groupData = true;
-console.log(dateAxis.groupCount);
+dateAxis.renderer.grid.template.location = 0;
+dateAxis.renderer.labels.template.fill = am4core.color("#e59165");
+dateAxis.groupData=true;
+dateAxis.groupCount=12;
+
+var dateAxis2 = chart.xAxes.push(new am4charts.DateAxis());
+dateAxis2.renderer.grid.template.location = 0;
+dateAxis2.renderer.labels.template.fill = am4core.color("#dfcc64");
+dateAxis2.groupData=true;
+dateAxis2.groupCount=12;
 
 var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis.tooltip.disabled = true;
+valueAxis.renderer.labels.template.fill = am4core.color("#e59165");
 
-// Create series
+valueAxis.renderer.minWidth = 60;
+
+var valueAxis2 = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis2.tooltip.disabled = true;
+valueAxis2.renderer.labels.template.fill = am4core.color("#dfcc64");
+valueAxis2.renderer.minWidth = 60;
+valueAxis2.syncWithAxis = valueAxis;
+
 var series = chart.series.push(new am4charts.LineSeries());
-series.dataFields.valueY = "value";
+series.name = "Measure1";
 series.dataFields.dateX = "date";
-series.tooltipText = "{value}"
+series.dataFields.valueY = "value";
+series.tooltipText = "{valueY.value}";
+series.fill = am4core.color("#e59165");
+series.stroke = am4core.color("#e59165");
+//series.strokeWidth = 3;
 
-series.tooltip.pointerOrientation = "vertical";
+var series2 = chart.series.push(new am4charts.LineSeries());
+series2.name = "Measure2";
+series2.dataFields.dateX = "date2";
+series2.dataFields.valueY = "value2";
+series2.yAxis = valueAxis2;
+series2.xAxis = dateAxis2;
+series2.tooltipText = "{valueY.value}";
+series2.fill = am4core.color("#dfcc64");
+series2.stroke = am4core.color("#dfcc64");
+//series2.strokeWidth = 3;
 
 chart.cursor = new am4charts.XYCursor();
-chart.cursor.snapToSeries = series;
-chart.cursor.xAxis = dateAxis;
+chart.cursor.xAxis = dateAxis2;
 
-chart.scrollbarX = new am4core.Scrollbar();
+var scrollbarX = new am4charts.XYChartScrollbar();
+scrollbarX.series.push(series);
+chart.scrollbarX = scrollbarX;
+
+chart.legend = new am4charts.Legend();
+chart.legend.parent = chart.plotContainer;
+chart.legend.zIndex = 100;
+
+valueAxis2.renderer.grid.template.strokeOpacity = 0.07;
+dateAxis2.renderer.grid.template.strokeOpacity = 0.07;
+dateAxis.renderer.grid.template.strokeOpacity = 0.07;
+valueAxis.renderer.grid.template.strokeOpacity = 0.07;
 
   } 
   }
